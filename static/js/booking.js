541667bea8fa30ce145a33e691804e8176a953b2
lark1987 <bosworth210920@gmail.com>
2023-09-26 14:39:24 +0800

    0926-1439

--- static/js/booking.js
+++ static/js/booking.js
@@ -15,6 +15,7 @@ test1Button.addEventListener("click", () => {
     .then(response => response.json())
     .then(data => {
         console.log(data);
+        getdata(data);
       })
     .catch(error => {
     console.log(error);
@@ -41,4 +42,25 @@ test2Button.addEventListener("click", () => {
     .catch(error => {
     console.log(error);
     });
+})
+
+// 預定資訊載入
+function getdata(data){
+    const attraction_picture = document.querySelector(".attraction_picture");
+    attraction_picture.setAttribute("src",data.data.attraction.image);
+    const attraction_name = document.querySelector(".attraction_name");
+    attraction_name.textContent="台北一日遊："+data.data.attraction.name;
+    const booking_date = document.querySelector(".booking_date");
+    booking_date.textContent="日期："+data.data.date;
+    const booking_time = document.querySelector(".booking_time");
+    booking_time.textContent="時間："+data.data.time;
+    const booking_price = document.querySelector(".booking_price");
+    booking_price.textContent="費用："+data.data.price;
+    const booking_address = document.querySelector(".booking_address");
+    booking_address.textContent="地點："+data.data.attraction.address;
+}
+
+//測試按鈕
+const test3Button = document.querySelector(".test3");
+test3Button.addEventListener("click", () => {
 })
