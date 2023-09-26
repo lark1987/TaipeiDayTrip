7dd260f15bb243bb5edd4f257aedb763e646f289
lark1987 <bosworth210920@gmail.com>
2023-09-26 15:52:37 +0800

    0926-1552

--- static/js/booking.js
+++ static/js/booking.js
@@ -1,8 +1,8 @@
 
-// 取得預定行程 (按鈕測試)
-const test1Button = document.querySelector(".test1");
-test1Button.addEventListener("click", () => {
-    
+getbooking()
+
+// 取得預定行程
+function getbooking(){
     let token = localStorage.getItem("token");
     const bookingUrl = "/api/booking";
     fetch(bookingUrl, {
@@ -20,11 +20,27 @@ test1Button.addEventListener("click", () => {
     .catch(error => {
     console.log(error);
     });
-})
+}
 
-// 刪除預定行程 (按鈕測試)
-const test2Button = document.querySelector(".test2");
-test2Button.addEventListener("click", () => {
+// 載入預定資訊
+function getdata(data){
+    const attraction_picture = document.querySelector(".attraction_picture");
+    attraction_picture.setAttribute("src",data.data.attraction.image);
+    const attraction_name = document.querySelector(".attraction_name");
+    attraction_name.textContent="台北一日遊："+data.data.attraction.name;
+    const booking_date = document.querySelector(".booking_date");
+    booking_date.textContent=data.data.date;
+    const booking_time = document.querySelector(".booking_time");
+    booking_time.textContent=data.data.time;
+    const booking_price = document.querySelector(".booking_price");
+    booking_price.textContent=data.data.price;
+    const booking_address = document.querySelector(".booking_address");
+    booking_address.textContent=data.data.attraction.address;
+}
+
+// 刪除預定行程
+const bookingDeleteButton = document.querySelector(".booking_delete");
+bookingDeleteButton.addEventListener("click", () => {
     
     let token = localStorage.getItem("token");
     const bookingUrl = "/api/booking";
@@ -44,21 +60,7 @@ test2Button.addEventListener("click", () => {
     });
 })
 
-// 預定資訊載入
-function getdata(data){
-    const attraction_picture = document.querySelector(".attraction_picture");
-    attraction_picture.setAttribute("src",data.data.attraction.image);
-    const attraction_name = document.querySelector(".attraction_name");
-    attraction_name.textContent="台北一日遊："+data.data.attraction.name;
-    const booking_date = document.querySelector(".booking_date");
-    booking_date.textContent="日期："+data.data.date;
-    const booking_time = document.querySelector(".booking_time");
-    booking_time.textContent="時間："+data.data.time;
-    const booking_price = document.querySelector(".booking_price");
-    booking_price.textContent="費用："+data.data.price;
-    const booking_address = document.querySelector(".booking_address");
-    booking_address.textContent="地點："+data.data.attraction.address;
-}
+
 
 //測試按鈕
 const test3Button = document.querySelector(".test3");
